412a4794cc6f5791b562845402e8f3520eaff447
SHUBHAM SHIVAJI LINGAYAT <115860658+shubham-lingayat@users.noreply.github.com>
2024-10-03 21:38:42 +0530

    Update useGif.js

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const API_KEY = 'neKkOKsaWSMEf07Jy0urAByoO7rMTIkL';
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
 function useGif(tag) {
